0f949dd4a3f6dee2dfb4f1118b35089ea5d5a4cf
Dunn <leroy.c.dunn@gmail.com>
2022-02-10 11:20:50 +0200

    feat: cat validation

--- src/cat/entities/cat.entity.ts
+++ src/cat/entities/cat.entity.ts
@@ -1,6 +1,19 @@
+import { IsNotEmpty, IsPositive, IsString } from "class-validator";
+
 export class Cat {
+
+  @IsString()
+  @IsNotEmpty()
   id: string
+
+  @IsString()
+  @IsNotEmpty()
   name: string
+
+  @IsPositive()
   age: number
+
+  @IsString()
+  @IsNotEmpty()
   colour: string
 }
